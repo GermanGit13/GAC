908c4ce73b8ea92a043a07e7da0e1540f429212c
GermanGit13 <99618445+GermanGit13@users.noreply.github.com>
2023-01-03 02:56:26 +0100

    Preparar vista detalle Brigde

---
+++ app/src/main/java/com/svalero/gac/ViewBrigdeActivity.java
@@ -0,0 +1,14 @@
+package com.svalero.gac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewBrigdeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_brigde);
+    }
+}
